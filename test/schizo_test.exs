828084f48bb195895335f5fedd91002d21ccb0c1
teerawat1992 <teerawat.lamanchart@gmail.com>
2015-09-24 22:39:18 +0700

    Add tests for Schizo module

--- test/schizo_test.exs
+++ test/schizo_test.exs
@@ -1,4 +1,16 @@
 defmodule SchizoTest do
   use ExUnit.Case
   doctest Schizo
+
+  test "uppercase doesn't change the first word" do
+    assert Schizo.uppercase("foo") == "foo"
+  end
+
+  test "uppercase converts the second word to uppercase" do
+    assert Schizo.uppercase("foo bar") == "foo BAR"
+  end
+
+  test "uppercase converts every other word to uppercase" do
+    assert Schizo.uppercase("foo bar baz whee") == "foo BAR baz WHEE"
+  end
 end
