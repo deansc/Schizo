9ed18daca534ce0d51842656b304f0f4f8a4f5fb
teerawat1992 <teerawat.lamanchart@gmail.com>
2015-09-24 22:58:32 +0700

    Define uppercase/1 method for Schizo module

--- lib/schizo.ex
+++ lib/schizo.ex
@@ -1,2 +1,16 @@
 defmodule Schizo do
+  def uppercase(string) do
+    string
+      |> String.split(" ")
+      |> Stream.with_index
+      |> Enum.map(&uppercase_every_other_word/1)
+      |> Enum.join(" ")
+  end
+
+  def uppercase_every_other_word({word, index}) do
+    cond do
+      rem(index, 2) == 0 -> word
+      rem(index, 2) == 1 -> String.upcase word
+    end
+  end
 end
